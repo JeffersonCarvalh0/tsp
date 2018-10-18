fa1a2a35a22c0ac44eea485d7d9147f640e2cff7
Jefferson Carvalho <jeffersonhcarvalho@gmail.com>
2018-10-18 16:26:13 -0300

    added mroe stuff

--- tsp.cpp
+++ tsp.cpp
@@ -1,21 +1,26 @@
 # include "tsp.h"
 # include <fstream>
 
-TSPInstance::TSPInstance(int size): size(0) {}
+TSPInstance::TSPInstance() { size = 0; }
 
-void TSPInstance::load(string &filename) {
-    std::ofstream file;
+void TSPInstance::load(std::string &filename) {
+    std::fstream file;
     file.open(filename);
-    string token;
+    std::string token;
 
     while (file >> token) {
         if (token == "DIMENSION") {
             file >> size;
-            mat = std::vector<std::vector<int>>(size, std::vector<int>(size));
+            data = std::vector<std::vector<int>>(size, std::vector<int>(size));
         }
 
         if (token == "NODE_COORD_SECTION") break;
     }
+
+    for (int i = 0; i < size; ++i)
+        for (int j = 0; j < size; ++j)
+            
+            
 
     file.close();
 }
--- tsp.h
+++ tsp.h
@@ -12,7 +12,7 @@ private:
 
 public:
     TSPInstance();
-    void load(string &fname);
+    void load(std::string &fname);
 };
 
 # endif
