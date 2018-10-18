868171d37b3f44890df74acf9b173b2d686447cb
Jefferson Carvalho <jeffersonhcarvalho@gmail.com>
2018-10-18 16:08:57 -0300

    Added some stuff

--- tsp.cpp
+++ tsp.cpp
@@ -1,3 +1,21 @@
 # include "tsp.h"
+# include <fstream>
 
+TSPInstance::TSPInstance(int size): size(0) {}
 
+void TSPInstance::load(string &filename) {
+    std::ofstream file;
+    file.open(filename);
+    string token;
+
+    while (file >> token) {
+        if (token == "DIMENSION") {
+            file >> size;
+            mat = std::vector<std::vector<int>>(size, std::vector<int>(size));
+        }
+
+        if (token == "NODE_COORD_SECTION") break;
+    }
+
+    file.close();
+}
--- tsp.h
+++ tsp.h
@@ -2,9 +2,17 @@
 # define TSP_H
 
 # include <cmath>
+# include <string>
+# include <vector>
 
 class TSPInstance {
-    
+private:
+    int size;
+    std::vector<std::vector<int>> data;
+
+public:
+    TSPInstance();
+    void load(string &fname);
 };
 
 # endif
